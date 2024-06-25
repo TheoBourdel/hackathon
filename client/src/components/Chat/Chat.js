3a4ddb9d0e254a302d7ca6b719c4c40c7c3e7126
marouane <marouane@MacBook-Air-3.local>
2024-06-25 15:06:57 +0200

    chat

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 import { Button, TextInput } from 'flowbite-react';
 import UserModal from '../Modal/modalUser';
+import messageService from '../../services/messageService';
 
 const socket = io('http://localhost:8000'); 
 
@@ -17,6 +18,8 @@ const Chat = () => {
       socket.emit('joinRoom', room);
     }
 
+    messageService.getMessages().then(setMessages)
+
     socket.on('message', (message) => {
       setMessages((prevMessages) => [...prevMessages, message]);
     });
@@ -30,27 +33,39 @@ const Chat = () => {
     setModalIsOpen(false);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     if (message.trim()) {
       const newMessage = { userId: userType === 'doctor' ? 1 : 2, content: message };
-      socket.emit('message', newMessage);
-      setMessage('');
+      try {
+        await messageService.createMessage(newMessage);
+        socket.emit('message', newMessage);
+        setMessage('');
+      } catch (error) {
+        console.error('Failed to send message', error);
+      }
     }
   };
-
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Chat Application</h1>
+    <div className="p-0">
+        <div className='mb-4'> 
+            <h1 className='text-custom-black text-xl font-semibold'>Chat</h1>
+            {
+            
+            userType && <p>conversation avec {userType === 'doctor' ? 'Le client' : 'Le medcin'}</p>
+            }
+        </div>
       {userType ? (
         <>
           <div className="chat-window border p-4 rounded">
-            <div className="messages overflow-y-auto h-64">
+            <div className="messages overflow-y-auto h-64 flex flex-col">
               {messages.map((msg, index) => (
                 <div
                   key={index}
                   className={`message p-2 my-2 rounded max-w-xs break-words ${
-                    msg.userId === (userType === 'doctor' ? 1 : 2) ? 'bg-gray-200 self-end text-right' : 'bg-blue-200 self-start text-left'
+                    msg.userId === (userType === 'doctor' ? 1 : 2) 
+                      ? 'bg-gray-200 self-end text-right ml-auto' 
+                      : 'bg-custom-orange text-white self-start text-left mr-auto'
                   }`}
                 >
                   {msg.content}
@@ -66,7 +81,7 @@ const Chat = () => {
               placeholder="Type your message here..."
               className="flex-1 mr-2"
             />
-            <Button type="submit" gradientDuoTone="greenToBlue">Send</Button>
+            <Button type="submit" className="bg-custom-orange">Send</Button>
           </form>
         </>
       ) : (
