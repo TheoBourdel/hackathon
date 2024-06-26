c3a58cf13693a8f9fba292727d1ee448884d22f8
marouane <marouane@MacBook-Air-4.local>
2024-06-26 12:13:27 +0200

    messages et creation de rapport pas encore terminé

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Layout from "./layouts/Layout/layout";
 import Report from "./components/Report/Report";
 import Chat from "./components/Chat/Chat";
 import Vocal from "./components/Vocal/Vocal";
+import Messages from "./components/Message/Message";
 
 function App() {
 
@@ -12,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="rapports" element={<Report />} />
-          <Route path="messages" element={<h1>Messages</h1>} />
+          <Route path="messages" element={<Messages />} />
           <Route path="chat" element={<Chat />} />
           <Route path="vocal" element={<Vocal />} />
         </Route>
