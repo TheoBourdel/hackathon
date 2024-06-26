2e51373bce160863e25e9f1a7f3a2bbaf068cf4c
marouane <marouane@MacBook-Air-4.local>
2024-06-26 12:13:27 +0200

    messages et creation de rapport pas encore terminé

---
+++ client/src/components/Message/FormModal.js
@@ -0,0 +1,128 @@
+import React, { useEffect, useState } from 'react';
+import { Button, Modal, Select, TextInput, Textarea } from 'flowbite-react';
+
+const FormModal = ({ isOpen, onRequestClose, onSubmit, currentuser, currentMessages, setSelectedMessages }) => {
+  const [category, setCategory] = useState('normal');
+  const [messages, setMessages] = useState(currentMessages);
+  const [user, setUser] = useState();
+  const [selectedMessages, setSelectedMessagesForm] = useState([]);
+  const [description, setDescription] = useState();
+
+
+  const handleCheckboxChange = (message) => {
+    const isSelected = selectedMessages.some(selectedMessage => selectedMessage.content === message.content);
+
+    if (isSelected) {
+      setSelectedMessagesForm(selectedMessages.filter(selectedMessage => selectedMessage.content !== message.content));
+      setSelectedMessages(selectedMessages.filter(selectedMessage => selectedMessage.content !== message.content));
+    } else {
+      setSelectedMessagesForm([...selectedMessages, message]);
+      setSelectedMessages([...selectedMessages, message]);
+    }
+  };
+
+  useEffect(() => {
+    if (currentuser) {
+      setUser(currentuser);
+    }
+
+    if (currentMessages) {
+      setMessages(currentMessages);
+    }
+  }, [currentuser, currentMessages]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(description, user, selectedMessages, category)
+    // onSubmit({ category });
+    // onRequestClose();
+  };
+
+  return (
+    <Modal show={isOpen} onClose={onRequestClose}>
+      <Modal.Header>
+        Select Category
+      </Modal.Header>
+      <Modal.Body>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            {user && (
+              <TextInput
+                value={user.username}
+                disabled
+                className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+              />
+            )}
+
+            <label htmlFor="category" className="block text-sm font-medium text-gray-700 mt-4">
+              Category
+            </label>
+            <Select
+              id="category"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+              required
+              className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+            >
+              <option value="normal">Normal</option>
+              <option value="urgent">Urgent</option>
+              <option value="tres urgent">Très Urgent</option>
+            </Select>
+
+            {messages.length !== 0 && (
+              <div className="p-4">
+                <label htmlFor="messages" className="block text-sm font-medium text-gray-700 mt-4">
+                  Les Messages
+                </label>
+                <div className="mt-1 block w-full max-h-40 overflow-y-auto border rounded-md">
+                  {messages.map((message, index) => (
+                    <div
+                      key={index}
+                      className={`p-2 cursor-pointer flex items-center justify-between ${
+                        selectedMessages.some(selectedMessage => selectedMessage.content === message.content)
+                          ? 'bg-blue-100 border-blue-500'
+                          : 'bg-white border-gray-300'
+                      } border-b hover:bg-gray-100`}
+                    >
+                      <label className="flex items-center space-x-3 w-full">
+                        <input
+                          type="checkbox"
+                          checked={selectedMessages.some(selectedMessage => selectedMessage.content === message.content)}
+                          onChange={() => handleCheckboxChange(message)}
+                          className="form-checkbox h-5 w-5 text-blue-600"
+                        />
+                        <span className="text-gray-700">{message.content}</span>
+                      </label>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
+
+            <div className="p-4">
+              <label htmlFor="description" className="block text-sm font-medium text-gray-700">
+                Description
+              </label>
+              <Textarea
+                onChange={(e) => setDescription(e.target.value)}
+                id="description"
+                className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+              />
+            </div>
+          </div>
+          <div className="flex justify-end">
+            <Button type="button" onClick={onRequestClose} color="gray" className="mr-2">
+              Cancel
+            </Button>
+            <Button type="submit" className="bg-custom-orange">
+              Generer
+            </Button>
+          </div>
+        </form>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default FormModal;
---
+++ client/src/components/Message/Message.js
@@ -0,0 +1,129 @@
+import React, { useEffect, useState } from 'react';
+import userService from '../../services/userService';
+import { Button } from 'flowbite-react';
+import UserList from './UserList';
+import { format } from 'date-fns';
+import FormModal from './FormModal';
+
+const Messages = () => {
+  const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [messages, setMessages] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [isUserSelectionned, setIsUserSelectionned] = useState(true);
+  const [user, setUser] = useState();
+  const openModal = () => setModalIsOpen(true);
+  const closeModal = () => setModalIsOpen(false);
+  const [selectedMessages, setSelectedMessages] = useState([]);
+
+
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const usersData = await userService.getUsers();
+        setUsers(usersData);
+      } catch (error) {
+        console.error('Error fetching users', error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  const handleFormSubmit = (formData) => {
+    console.log('Form submitted with category:', formData.category);
+    // Traitement supplémentaire du formulaire ici
+  };
+
+  useEffect(() => {
+
+    if(selectedMessages.length !=0) {
+      console.log(selectedMessages)
+    }
+
+
+  }, [selectedMessages])
+
+  const handleUserClick = async (userId) => {
+    try {
+      const userMessages = await userService.getUserMessages(userId);
+      const user = await userService.getUser(userId);
+
+      if(user && userMessages) {
+        console.log(user, userMessages)
+        setUser(user)
+        setMessages(userMessages);
+        setIsUserSelectionned(false)
+        setSelectedUser(userId);
+      }
+
+    } catch (error) {
+      console.error('Error fetching messages for user', error);
+    }
+  };
+
+  return (
+    <>
+    <div className="flex justify-between  ">
+        <div className='mb-4'>
+            <h1 className='text-custom-black text-xl font-semibold'>Messages</h1>
+            <p>Liste des utilisateurs</p>
+        </div>
+        <div className="">
+            <Button type="submit" disabled={isUserSelectionned} className="bg-custom-orange" onClick={openModal} >Generer le rapport</Button>
+        </div>
+    </div>
+    <FormModal setSelectedMessages={setSelectedMessages} isOpen={modalIsOpen} onRequestClose={closeModal} onSubmit={handleFormSubmit} currentuser={user} currentMessages={messages} />
+
+    <div className="flex h-screen">
+      <div className="w-1/3 bg-gray-50 p-4 border-r">
+        <div className="user-list">
+          {users.map((user) => (
+            <div
+              key={user.id}
+              className="user p-2 my-2 mx-2  border-b border-gray-400 flex items-center cursor-pointer"
+              onClick={() => handleUserClick(user.id)}
+            >
+              <img
+                src={`https://ui-avatars.com/api/?name=${user.username}`}
+                alt="profile"
+                className="w-10 h-10 rounded-full mr-2"
+              />
+              <span className="font-semibold">{user.username}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="w-2/3 pl-4 bg-gray-00 ">
+        <div className="messages-card p-4 border rounded h-3/4">
+          <h2 className="text-xl font-bold mb-2">Messages</h2>
+          {selectedUser ? (
+            messages.length > 0 ? (
+              messages.map((msg, index) => (
+                <div
+                  key={index}
+                  className={`message p-2 my-2 rounded max-w-xs break-words
+                      bg-custom-orange text-white self-start text-left mr-auto`}
+                >
+                  {msg.content}
+                  <div className="timestamp text-xs text-white-300">
+                    {format(new Date(msg.timestamp), 'MMMM dd, yyyy, h:mm a')}
+                  </div>
+                </div>
+              ))
+            ) : (
+              <p>No messages</p>
+            )
+          ) : (
+            <p>Selectionner un utilisateur pour voir ses messages</p>
+          )}
+        </div>
+  
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default Messages;
